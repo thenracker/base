8731e6f666a289e721984ba3c70fa611e47308ca
Petr Weissar <weissar.petr@gmail.com>
2020-03-07 21:08:04 +0100

    Fix: New dto

--- app/src/main/java/cz/example/base/ui/schedule/ScheduleAdapter.kt
+++ app/src/main/java/cz/example/base/ui/schedule/ScheduleAdapter.kt
@@ -3,14 +3,15 @@ package cz.example.base.ui.schedule
 import android.content.Context
 import android.view.View
 import cz.example.base.R
-import cz.example.base.data.rest.dto.CalendarScheduleItemResponse
+import cz.example.base.data.rest.dto.DummyResponse
 import cz.example.base.ui.base.BaseListAdapterBuilder
 
-class ScheduleAdapter(context: Context, private val onClick: (CalendarScheduleItemResponse) -> Unit) : BaseListAdapterBuilder<CalendarScheduleItemResponse>(context) {
+class ScheduleAdapter(context: Context, private val onClick: (DummyResponse) -> Unit) :
+    BaseListAdapterBuilder<DummyResponse>(context) {
 
     override fun getRowLayout() = R.layout.row_schedule
 
-    override fun View.onBind(item: CalendarScheduleItemResponse) {
+    override fun View.onBind(item: DummyResponse) {
         // ToDo bind
 
         setOnClickListener {
